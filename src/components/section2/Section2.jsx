acf4cf3d73b18b1c41e9dc60e14f61d07c231b93
Leonardo <novaesleo2005@gmail.com>
2024-09-17 22:02:16 -0300

    Section 2

--- src/components/section2/Section2.jsx
+++ src/components/section2/Section2.jsx
@@ -1,9 +1,21 @@
-import './Section2.css'
-function Section2 (){
-    return(
-    <>
-    <div className="Container2"></div>
-    </>
-    )
+import './Section2.css';
+import CardCarro from '../../assets/CardCarro.png';
+
+function Section2() {
+    return (
+        <div className="container2">
+            <div className="text-section">
+                <h1>O que é a Formula-E?</h1>
+                <p>
+                    Fórmula E é mais do que apenas uma série de corridas - é uma batalha pelo futuro.
+                    Nossos carros radicais - todos movidos à eletricidade - abrem caminho para os carros de rua de amanhã.
+                </p>
+            </div>
+            <div className="image-section">
+                <img src={CardCarro} alt="Carro de Fórmula E" />
+            </div>
+        </div>
+    );
 }
-export default Section2
+
+export default Section2;
