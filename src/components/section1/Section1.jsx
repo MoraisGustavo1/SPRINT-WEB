ba4449e9ab79c7807e91fa42b4264afc75c076ba
Leonardo <novaesleo2005@gmail.com>
2024-09-18 20:03:05 -0300

    .

--- src/components/section1/Section1.jsx
+++ src/components/section1/Section1.jsx
@@ -3,7 +3,7 @@ function Section1 (){
     return(
         <>
         <div className="Container1">
-            <div className='Teste'>
+            <div className='FundoFosco'>
                 <h1>Fórmula E: A Corrida do Futuro</h1>
             </div>
         </div>
--- src/components/section3/Section3.jsx
+++ src/components/section3/Section3.jsx
@@ -2,7 +2,14 @@ import './Section3.css'
 function Section3 () {
     return(
         <>
-        <div className="Container3"></div>
+        <div className="Container3">
+            <div className='FundoFosco'>
+                
+                    <h3>Nossos desafios e Soluções</h3>
+                    <p>A Fórmula E enfrenta desafios significativos para se destacar no mercado saturado de esportes automobilísticos. Apesar de seu foco em sustentabilidade, educação e inovação, a categoria não atinge o nível desejado de popularidade e engajamento. O público-alvo, especialmente os jovens e entusiastas de tecnologia, busca experiências mais interativas e imersivas.</p>
+            
+            </div>
+        </div>
         </>
     )
 }
