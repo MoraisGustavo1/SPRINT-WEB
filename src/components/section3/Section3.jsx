62505c629272911373ba7010d0cd6e09f0cf1b47
Leonardo <novaesleo2005@gmail.com>
2024-09-18 22:30:52 -0300

    Commit final 18/09/2024

--- src/components/section3/Section3.jsx
+++ src/components/section3/Section3.jsx
@@ -7,7 +7,7 @@ function Section3 () {
     return(
         <>
         <div className="Container3">
-            <div className='FundoFosco'>
+            <div className='Fundo'>
                 
                     <h3>Nossos desafios e Soluções</h3>
                     <p>A Fórmula E enfrenta desafios significativos para se destacar no mercado saturado de esportes automobilísticos. Apesar de seu foco em sustentabilidade, educação e inovação, a categoria não atinge o nível desejado de popularidade e engajamento. O público-alvo, especialmente os jovens e entusiastas de tecnologia, busca experiências mais interativas e imersivas.</p>
--- src/components/section4/Section4.jsx
+++ src/components/section4/Section4.jsx
@@ -1,8 +1,18 @@
 import './Section4.css'
+import Pessoas from '../../assets/Pessoas.png'
 function Section4 (){
     return(
         <>
-        <div className="Container4"></div>
+        <div className="Container4">
+            <div className='Sec4Img'><img src={Pessoas} alt="" /></div>
+            <div className='Textos'>
+                <h3>A Proxima Temporada da Formula E</h3>
+                <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente sint odio, maxime quis corrupti reiciendis quibusdam possimus, modi dolorem at repellat nobis quas impedit debitis dolor soluta explicabo culpa! Doloribus!
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas, odio. Tenetur voluptate debitis dolore voluptatem facilis magni assumenda cupiditate, voluptatibus ipsam numquam itaque omnis eaque qui. Non velit in recusandae?
+                </p>
+            </div>
+        </div>
         </>
     )
 }
